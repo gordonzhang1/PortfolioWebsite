abeac3c4b3a9aff8e9db274acbca6590c8eb4521
Gordon06060 <gordonz0913@gmail.com>
2025-01-03 02:17:33 -0800

    fix: artex title

--- src/components/TracingBeamDemo.jsx
+++ src/components/TracingBeamDemo.jsx
@@ -78,7 +78,7 @@ const dummyContent = [
       "https://images.unsplash.com/photo-1519681393784-d120267933ba?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    title: "Software Curriculum Developer & Associate Founder",
+    title: "Software Curriculum Developer & Co-Founder",
     subtitle: "ARTex Coding Society | Jan 2023 - May 2024",
     description: (
       <>
