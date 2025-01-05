afe90cc6e0830cb384c3f0a60fe2ce8e39be9d1b
Gordon06060 <gordonz0913@gmail.com>
2025-01-04 23:31:08 -0800

    nav spacing

--- src/components/ui/navbar-menu.jsx
+++ src/components/ui/navbar-menu.jsx
@@ -27,7 +27,7 @@ export const MenuItem = ({ setActive, active, item, children }) => {
           transition={transition}
         >
           {active === item && (
-            <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
+            <div className="absolute top-[calc(100%_+_0.1rem)] left-1/2 transform -translate-x-1/2 pt-4">
               <motion.div
                 transition={transition}
                 layoutId="active"
